556e982aa1c0c9c010b6cff27f687da25f0e14b0
shajib126 <shajib126@gmail.com>
2023-02-22 21:04:34 +0600

    file limit added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const taskRoute = require('./routers/TaskRoutes')
 const app = express()
 
 //dep middlware
-app.use(express.json())
+app.use(express.json({limit:'50mb'}))
 app.use(cors())
 dotenv.config()
 app.use(morgan('common'))
